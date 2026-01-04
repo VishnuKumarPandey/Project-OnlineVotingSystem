661aaeb3648cb21c48c9fbc62e23dcd3053df5dd
Vishnu Kumar Pandey <gitforv12345@gmail.com>
2026-01-04 11:20:27 +0530

    Changes in the project in html and css

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
-let votes = {}; 
+let votes = {};
 
 function vote() {
     const name = document.getElementById('name').value;
-    
     const selectedCandidate = document.querySelector('input[name="Candidate"]:checked');
     const messageDiv = document.getElementById('message');
 
-    
     if (!name || !selectedCandidate) {
-        messageDiv.innerHTML = "<span style='color: red;'>Please enter your name and select a candidate.</span>";
+        messageDiv.innerHTML = "<p style='color: yellow;'>Error: Enter your name and select the party!</p>";
         return;
     }
 
+    votes[name] = selectedCandidate.value;
+    messageDiv.innerHTML = `<p style='color: #dfe4dfff;'>Thank You ${name}! Your Vote has been recorded.</p>`;
     
-    const candidateName = selectedCandidate.value;
-    votes[name] = candidateName;
-    
-    messageDiv.innerHTML = `Thank you, <strong>${name}</strong>, for voting for ${candidateName}!`;
-
     document.getElementById('name').value = "";
     selectedCandidate.checked = false;
 }
 
 function calculateVotes() {
     const resultDiv = document.getElementById('result');
-    let voteCounts = { "BJP": 0, "Congress": 0, "Other": 0 };
-    let resultHTML = "<h4>Voting Result:</h4>";
-
+    let voteCounts = { "BJP": 0, "Congress": 0, "BSP": 0, "Samajwadi": 0, "CPI": 0, "AITC": 0, "AAP": 0, "JDU": 0 };
+    
     
     for (let voter in votes) {
         let party = votes[voter];
@@ -35,29 +29,34 @@ function calculateVotes() {
         }
     }
 
+    
     let winner = "";
-    let maxVotes = -1;
-    let tie = false;
+    let maxVotes = 0;
+    let isTie = false;
 
     for (let party in voteCounts) {
-        resultHTML += `<p>${party}: ${voteCounts[party]} votes</p>`;
-        
         if (voteCounts[party] > maxVotes) {
             maxVotes = voteCounts[party];
             winner = party;
-            tie = false;
+            isTie = false;
         } else if (voteCounts[party] === maxVotes && maxVotes > 0) {
-            tie = true;
+            isTie = true;
         }
     }
 
-    if (maxVotes <= 0) {
-        resultHTML += "<h3>No votes cast yet.</h3>";
-    } else if (tie) {
-        resultHTML += "<h3>Result: It's a Tie!</h3>";
-    } else {
-        resultHTML += `<h2 style="color: #4c4c4fff;">Winner: ${winner}!</h2>`;
-    }
     
+    let resultHTML = "<h3>--- Result ---</h3>";
+    for (let party in voteCounts) {
+        resultHTML += `<p>${party}: ${voteCounts[party]} votes</p>`;
+    }
+
+    if (maxVotes === 0) {
+        resultHTML += "<h3>No voting done yet!</h3>";
+    } else if (isTie) {
+        resultHTML += "<h2 style='color: orange;'>Result: Tie !</h2>";
+    } else {
+        resultHTML += `<h2 style='color: #313638ff; background: #333; padding: 10px; border-radius: 10px;'>Winner : ${winner} </h2>`;
+    }
+
     resultDiv.innerHTML = resultHTML;
 }
